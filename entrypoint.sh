0d3c6ef6cfc8c0588020c26e1a7071320eb07b21
Veronelli <facu2000veronelli@outlook.com>
2025-05-10 03:28:38 +0000

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+pip install -r requirements.txt
+poetry install --no-root
+
+echo "PostgreSQL started"
+
+# Ejecutar migraciones
+poetry run python django_prog/manage.py migrate
+
+# Ejecutar el comando que se pase como argumento
+exec "$@"
