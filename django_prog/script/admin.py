cb8354d73d9183416f0903db3598e29b37f1d123
Veronelli <facu2000veronelli@outlook.com>
2025-05-24 04:22:04 +0000

    add: script

---
+++ django_prog/script/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django_prog.script.models import Script
+
+
+@admin.register(Script)
+class ScriptAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
--- django_prog/settings.py
+++ django_prog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_prog.script'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
